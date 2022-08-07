4d6ff6cdcc285d8bdf1c40bc178a6fb3d6b1cb9d
kurehajime <xiidec@gmail.com>
2022-08-07 21:21:23 +0900

    HTML生成

--- qrray.go
+++ qrray.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
+	"html/template"
+	"io"
+	"log"
 	"path/filepath"
 	"strings"
 	"sync"
 
 	qrcode "github.com/skip2/go-qrcode"
 )
+
+//go:embed pages/index.html
+var index string
+
+func ToHtml(lines string,prefix string,size int,writer io.Writer) {
+    tpl, err := template.New("").Parse(index)
+	if err != nil {
+		panic(err)
+	}
+    if err = tpl.Execute(writer, struct {
+		Size   int
+		Prefix string
+		Count    int
+	}{
+		Size:   size,
+		Prefix: prefix,
+		Count:    len(strings.Split(lines, "\n")),
+	}); err != nil {
+        log.Fatal(err)
+    }
+}
 
 func Creates(lines string,prefix string,size int,dir string) {
 	arr := strings.Split(lines, "\n")
