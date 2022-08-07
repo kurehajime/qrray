96b1f5bcaebff7d7db649f4e2256c5872a5b6675
kurehajime <xiidec@gmail.com>
2022-08-07 22:05:21 +0900

    main.go

--- main.go
+++ main.go
@@ -2,69 +2,88 @@ package main
 
 import (
 	"bufio"
+	"flag"
+	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 )
 
 func main() {
-	prefix := "qrray_test_"
-	size := 128
+	var text string
+	var err error
+	var size int
+	var prefix string
+	var out string
 
-	input := `https://github.com/kurehajime/qrray
-いつもそう　いいわけばかり　いうじぶん　 
-ろこつなたいどで　きょひられる 
-はばつにぞくさず　ひとりでこりつ 
-にてもやいても　くえないやつだ 
-ほけつのほけつ　あまりもの 
-とけいといつも　にらめっこ 
-ちかくて　とおい　おとなりさん 
-りくつはいちりゅう　こうどうさんりゅう 
-ぬくぬくと　あまやかされて　いまがある 
-るーるをまもらず　じぶんをまもる 
-をいをいと　あきれられても　きにしない 
-わからない　わからないと　げんじつとうひ 
-からだはおとな　なかみはこども 
-よそではいいこ　うちべんけい 
-たべてねる　そのくりかえし　わがじんせい 
-れべるが　ちがう　うまれつき 
-そいつだれ　かげがうすくて　めだたない 
-つづかない　ふけいぞくは　ひりきなり 
-ねてもおきても　あそんでばかり 
-なかよしこよし　うわべだけ 
-らいねんこそは　がんばるぞ　きょねんも　おなじ　もくひょうたてた 
-むりをせず　どりょくもせずに　なにもせず 
-うしろゆび　さされるのには　もうなれた 
-ゐいかげん　やるきだしたら　どうだろう 
-のとなれ　やまとなれ　せんぞのとち 
-おさいふの　でいりがはげしい　ろうひぐせ 
-くろうした　けいけんないから　げんじつはくり 
-やばいやばい　ききかんあるふり　くちはっちょう 
-まけいぬのなかの　まけいぬ 
-けいさつの　おせわにだけは　なりたくない 
-ふこうじまんは　とくいわざ 
-こきょうに　にしきを　かざれない 
-えいきょうを　すぐにうけちゃう　たんさいぼう 
-てんどうと　よばれていたのは　ごさいまで 
-あすもまた　おなじせいかつ　あさっても 
-さきゆきが　まったくみえない　ごりむちゅう 
-きょうできることは　あしたやる 
-ゆめがない　きぼうもなければ　かねもない 
-めそめそと　おんながくさった　ようなやつ 
-みてみぬふり　せいぎかんは　みじんもない 
-しにたいと　しぬきがないのに　れんこする 
-よのなかで　やっていけない　ふていごうしゃ 
-ひがくれるまで　ぱそこんざんまい 
-もたもたと　しごとはおそいが　かえるのはやい 
-せけんしらずで　はじしらず 
-すぐにねをあげ　しろはたあげる`
-	Creates(input, prefix, size, filepath.Base("./out"))
+	flag.IntVar(&size, "size", 128, "size of qr code")
+	flag.StringVar(&prefix, "prefix", "qr_", "file name prefix")
+	flag.StringVar(&out, "out", "./qr", "out put path")
+	flag.Parse()
+	
+	if f, err := os.Stat(out); os.IsNotExist(err) || !f.IsDir() {
+		if err := os.Mkdir(out, 0777); err != nil {
+			panic(err)
+		}
+	}
 
-	f, err := os.Create("out/index.html")
+	if len(flag.Args()) == 0 {
+		text = readPipe()
+	} else if flag.Arg(0) == "-" {
+		text = readStdin()
+	} else {
+		text= readFileByArg(flag.Arg(0))
+	}
+	if(text == ""){
+		fmt.Fprintln(os.Stderr, "Specify a text file as an argument.")
+		return
+	}
+
+	// create qr code
+	Creates(text, prefix, size, filepath.Base(out))
+
+	// create index.html
+	f, err := os.Create(filepath.Join(out,"index.html"))
 	if err != nil {
 		panic(err)
 	}
 	writer := bufio.NewWriter(f)
-	ToHtml(input, prefix, size,writer )
+	ToHtml(text, prefix, size,writer )
 	writer.Flush()
 	defer f.Close()
+}
+
+func readPipe() string {
+	stats, _ := os.Stdin.Stat()
+	if stats != nil && (stats.Mode()&os.ModeCharDevice) == 0 {
+		bytes, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			panic(err)
+		}
+		return string(bytes)
+	} else {
+		return ""
+	}
+}
+func readStdin() string {
+	var text string
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		if s.Text() == "" {
+			break
+		}
+		text += s.Text() + "\n"
+	}
+	if s.Err() != nil {
+		panic(s.Err())
+	}
+	return text
+}
+
+func readFileByArg(path string) string {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	return string(content)
 }
